4738f6c3e3665a0933dda443272176f817aeeedd
victorclf <victorclf@live.com>
2016-01-15 09:47:01 -0300

    Add Person.java

---
+++ somecompany/someprogram/person/Person.java
@@ -0,0 +1,22 @@
+package somecompany.someprogram.person;
+
+class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return firstName;
+    }
+
+    public void setLastName(String firstName) {
+        this.firstName = firstName;
+    }
+}
